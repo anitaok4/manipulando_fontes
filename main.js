abefa33d614fdb140c15ed8bbb8314a5f91a3992
anitaok4 <114119009+anitaok4@users.noreply.github.com>
2023-11-28 21:31:53 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function setup(){
+    video = createCapture(VIDEO)
+    video.size(550, 550);
+    canvas = createCanvas(550, 550)
+    canvas.position(560,150)
+    poseNet = ml5.poseNet(video, modelLoaded)
+    poseNet.on('pose', gotPoses)
+}
+
+function draw(){
+    background('#BBE8B2')
+
+}
+
+function modelLoaded(){
+    console.log("deu certo")
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+    }
+}
